8a6f4b3da5715f4ad8515d18e86797f3f760ac3c
Sydney Corbett <17sc98@queensu.ca>
2019-03-31 11:45:40 -0400

    Main Menu

--- Mathgame/app/src/main/java/com/essdev/russ/mathgame/MainActivity.java
+++ Mathgame/app/src/main/java/com/essdev/russ/mathgame/MainActivity.java
@@ -3,11 +3,15 @@ package com.essdev.russ.mathgame;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.MenuInflater;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -17,7 +21,8 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.menu.main_menu);
+
 
         GameView game = (GameView) findViewById(R.id.gameView);
 
@@ -34,4 +39,33 @@ public class MainActivity extends Activity {
 
 //        game.setEditText(editText);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.stats:
+                Intent intent1 = new Intent(this, Statistics.class);
+                this.startActivity(intent1);
+                return true;
+            case R.id.about:
+                Intent intent2 = new Intent(this, About.class);
+                this.startActivity(intent2);
+                return true;
+            case R.id.play:
+                Intent intent3 = new Intent(this, GameView.class);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 }
